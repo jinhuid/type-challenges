68a25473bfd33e097415cb2c16855181731ff162
huiddd <1732122592@qq.com>
2024-05-11 11:39:04 +0800

    feat: Add SimpleVue implementation with data, computed, and methods

---
+++ 00057-hard-get-required.ts
@@ -0,0 +1,23 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils"
+
+type cases = [
+  Expect<Equal<GetRequired<{ foo: number; bar?: string }>, { foo: number }>>,
+  Expect<
+    Equal<GetRequired<{ foo: undefined; bar?: undefined }>, { foo: undefined }>
+  >
+]
+
+// ============= Your Code Here =============
+type GetRequired<T> = {
+  [P in keyof T as T[P] /**这里的T[P]如果可选类型 那么T[P]就是P|undefined 判断不成立 */ extends Required<T>[P]
+    ? P
+    : never]: T[P]
+}
+
+type aa<T> = { [k in keyof T]: k }
+type a = GetRequired<{ foo: number; bar?: string }>
+
+type b = aa<{ a?: 123 }>
+
+type c = "a" | undefined extends "a" ? true : false
